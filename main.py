7cfccd55b9cf9566a586457270a99c570f2e6cde
cwkang <cwk1998@hotmail.com>
2018-08-02 04:39:08 +0800

    fix bug where colons are causing files to not be saved properly by replacing colons with double dashes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-import sys
+import re
 from urllib.request import urlopen, Request
 import pdfkit
 from bs4 import BeautifulSoup
@@ -7,7 +7,8 @@ from bs4 import BeautifulSoup
 def main(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     soup = BeautifulSoup(urlopen(req), features="html.parser")
-    pdfkit.from_url(url, f"./saved_pages/{soup.title.string}.pdf")
+    file_name = soup.title.string.replace(":", "--") + ".pdf"
+    pdfkit.from_url(url, f"./saved_pages/{file_name}")
 
 if __name__ == '__main__':
     try:
