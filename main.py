07a252d25129e26b74e76994ef187086aa7a12b7
cwkang <cwk1998@hotmail.com>
2018-08-02 04:16:13 +0800

    fix wrong placement of 'feature' argument for Beautiful soup

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import pdfkit
 from bs4 import BeautifulSoup
 
 def main(url):
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'}, features="html.parser")
-    soup = BeautifulSoup(urlopen(req))
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(urlopen(req), features="html.parser")
     pdfkit.from_url(url, f"./saved_pages/{soup.title.string}.pdf")
 
 if __name__ == '__main__':
     try:
         if(len(sys.argv) >= 2):
-            for i in range(1, len(sys.argv)):
-                main(sys.argv[i])
+            for url in sys.argv[1:]:
+                main(url)
         else:
             print("Please provide a single url as an argument.")
     except ValueError as e:
